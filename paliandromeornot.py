607e504c8625ab503fdff2f49e2dedf065c30615
sachuprasadrs <sachuprasadrs@gmail.com>
2024-10-14 15:18:20 +0530

    Update paliandromeornot.py

--- paliandromeornot.py
+++ paliandromeornot.py
@@ -1,16 +1,13 @@
-def is_palindrome(n):
-     n_str = str(n)
-     n_rev = n_str[::-1]
-     if n_str == n_rev:
-         return True
-     else:
-         return False
-
-n = int(input("Enter a number to check if it's a palindrome: "))
-
-result = is_palindrome(n)
-
-if result:
-     print("The number is a palindrome.")
+def is_paliandrome(n):
+	n_str=str(n)
+	n_rev=n_str[::-1]
+	if n_str==n_rev:
+		return True
+	else:
+		return False
+n=input("Enter a value\n")
+result=is_paliandrome(n);
+if result==True:
+	print("The value is paliandrome")
 else:
-     print("The number is not a palindrome.")
+	print("The value is not paliandrome")
