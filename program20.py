13326d5e69da18036d89c8c71487aad32f5c81e9
sachuprasadrs <sachuprasadrs@gmail.com>
2024-10-25 11:20:45 +0530

    Update program20.py

--- program20.py
+++ program20.py
@@ -1,10 +1,11 @@
 n = int(input("Enter the number of words: "))
 words = []
 for i in range(n):
-     word = input("Enter a word: ")
-     words.append(word)
+	word = input("Enter a word: ")
+	words.append(word)
 longest_word = words[0]
 for word in words:
-     if len(word) > len(longest_word):
-         longest_word = word
-print("The word with the longest length is:", longest_word)
+	if len(word) > len(longest_word):
+		longest_word = word
+lar=len(longest_word)
+print("The word with the longest length is:", longest_word,"length:",lar)
