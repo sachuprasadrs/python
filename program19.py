3f033b6d1df57dbe1aa3836495b64073add5a9b0
sachuprasadrs <sachuprasadrs@gmail.com>
2024-10-25 11:30:48 +0530

    Update program19.py

--- program19.py
+++ program19.py
@@ -1,8 +1,5 @@
 string = input("Enter a string: ")
-
 start = string[0]
 end = string[-1]
-
 new_string = end + string[1:-1] + start
-
 print("String with first and last characters exchanged:", new_string)
